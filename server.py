e566db834876d06e339f8c10541553b9e70c2efe
skylartwinig <skylartwinig@umail.ucsb.edu>
2023-06-07 17:07:10 -0700

    solved leader election

--- server.py
+++ server.py
@@ -57,9 +57,8 @@ class Blog:
         return comments
 
 
-
 class Server:
-    def __init__(self,serverId):
+    def __init__(self, serverId):
         self.serverID = serverId
         self.accepted_proposal = None
         self.log = []
@@ -69,20 +68,20 @@ class Server:
         self.accepted_value = None
         self.proposal_value = None
         self.num_servers = 5
+        self.num_of_promises = 0
 
         self.my_socket = None
-        self.client1Sock = None
-        self.client2Sock = None
-        self.client3Sock = None
-        self.client4Sock = None
+        self.clientSockets = []
         self.clientSockPortList = [('localhost', 4445), ('localhost', 4446), ('localhost', 4447), ('localhost', 4448), ('localhost', 4449)]
         self.portNum = self.clientSockPortList[int(sys.argv[1]) - 1]
         self.clientSockPortList.remove(self.portNum)
 
         self.leaderId = None
 
-    def start(self):
+        self.blog = Blog()
+        self.pending_operations = []
 
+    def start(self):
         self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.my_socket.bind(self.portNum)
@@ -90,23 +89,21 @@ class Server:
 
         print(self.my_socket)
 
-        terminal = threading.Thread(target= self.get_user_input)
+        terminal = threading.Thread(target=self.get_user_input)
         terminal.start()
+
         while True:
-            # Accept client connections
             client_socket, client_address = self.my_socket.accept()
             print(client_socket, " why is this same port ", client_address)
 
-            # Create new thread for each client connection
             client_thread = threading.Thread(target=self.handle_client, args=(client_socket,))
             client_thread.start()
-        
+
     def get_user_input(self):
         while True:
-            user_input = input()
+            user_input = input().lower()
 
-            # Handle balance message
-            if user_input.startswith('Conn'):
+            if user_input.startswith('conn'):
                 self.client1Sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 self.client1Sock.connect(self.clientSockPortList[0])
                 self.client2Sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -115,35 +112,171 @@ class Server:
                 self.client3Sock.connect(self.clientSockPortList[2])
                 self.client4Sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 self.client4Sock.connect(self.clientSockPortList[3])
-            
-            elif user_input.startswith('POST'):
+
+            elif user_input.startswith('post'):
                 if not self.leaderId:
-                    self.leaderElection()
-            
+                    self.leader_election()
+
+                operation = user_input.split(' ', 1)[1]
+                self.pending_operations.append(operation)
+
+                #send to leader then send accept
+                if self.leaderId == self.serverID: #if leader send accept messages to all
+                    self.log.append(operation)
+                    self.accepted_proposal = (0, self.serverID, operation)
+                    self.accepted_round = 0
+                    self.accepted_value = operation
+                    self.proposal_value = operation
+                    self.num_of_promises = 1
+
+                    accept_message = 'ACCEPT ' + str((self.accepted_round, self.serverID, self.accepted_value))
+                    self.client1Sock.sendall(bytes(accept_message, 'utf-8'))
+                    self.client2Sock.sendall(bytes(accept_message, 'utf-8'))
+                    self.client3Sock.sendall(bytes(accept_message, 'utf-8'))
+                    self.client4Sock.sendall(bytes(accept_message, 'utf-8'))
+                else: #if not leader then send the message to the leader
+
+                    accept_message = 'TO LEADER ' + str((self.accepted_round, self.serverID, self.accepted_value))
+                    self.client1Sock.sendall(bytes(accept_message, 'utf-8'))
+                    self.client2Sock.sendall(bytes(accept_message, 'utf-8'))
+                    self.client3Sock.sendall(bytes(accept_message, 'utf-8'))
+                    self.client4Sock.sendall(bytes(accept_message, 'utf-8'))
+                
+            elif user_input.startswith('blog'):
+                if self.blog:
+                    print(self.blog.view_all_posts())
+                else:
+                    print("BLOG EMPTY")
+                
+            #
+            # handle "COMMENT"
+            #
+
             else:
                 self.client1Sock.sendall(bytes(user_input, 'utf-8'))
                 self.client2Sock.sendall(bytes(user_input, 'utf-8'))
                 self.client3Sock.sendall(bytes(user_input, 'utf-8'))
                 self.client4Sock.sendall(bytes(user_input, 'utf-8'))
 
-    def leaderElection(self):
-        ballot_number = 0
-        self.client1Sock.sendall(bytes('PREPARE ' + self.leaderId, 'utf-8'))
-        self.client2Sock.sendall(bytes('PREPARE ' + self.leaderId, 'utf-8'))
-        self.client3Sock.sendall(bytes('PREPARE ' + self.leaderId, 'utf-8'))
-        self.client4Sock.sendall(bytes('LEAPREPAREDER ' + self.leaderId, 'utf-8'))
+    def leader_election(self):
+        ballot_number = (0, self.serverID, len(self.log))
+        ballot_number = str(ballot_number).replace(' ', '')
+        prepare_message = 'PREPARE ' + ballot_number
+        print(prepare_message, " prepare message")
+        
+        self.client1Sock.sendall(bytes(prepare_message, 'utf-8'))
+        self.client2Sock.sendall(bytes(prepare_message, 'utf-8'))
+        self.client3Sock.sendall(bytes(prepare_message, 'utf-8'))
+        self.client4Sock.sendall(bytes(prepare_message, 'utf-8'))
 
-    
     def handle_client(self, client_socket):
         while True:
             sleep(3)
             data = client_socket.recv(1024).decode()
             print(data)
 
+            if data.startswith('PREPARE'):
+                ballot_number = eval(data.split(' ', 1)[1])
+                print(ballot_number)
+                if self.is_higher_ballot_number(ballot_number):
+                    self.leaderId = ballot_number[1]
+                    self.promised_round = ballot_number
+                    ballot_number = str(ballot_number).replace(' ', '')
+                    promise_message = 'PROMISE ' + ballot_number + ' ' + str(self.accepted_round) + ' ' + str(self.accepted_value)
+                    print("sending promise message: " + promise_message)
+                    self.client1Sock.sendall(bytes(promise_message, 'utf-8'))
+                    self.client2Sock.sendall(bytes(promise_message, 'utf-8'))
+                    self.client3Sock.sendall(bytes(promise_message, 'utf-8'))
+                    self.client4Sock.sendall(bytes(promise_message, 'utf-8'))
+
+            
+            elif data.startswith('PROMISE'):
+                promise_data = data.split(' ', 3)
+                print(promise_data[1])
+                ballot_number = eval(promise_data[1])
+                # ballot_number = promise_data[1]
+                print(ballot_number[1] , " ", self.serverID)
+                if ballot_number[1] == self.serverID:
+                    self.num_of_promises += 1
+                    if self.num_of_promises >3:
+                        self.leaderId = self.serverID
+                        print("I AM THE LEADER")
+                
+
+            # elif data.startswith('PROMISE'):
+            #     promise_data = data.split(' ', 3)
+            #     ballot_number = eval(promise_data[1])
+            #     # if self.is_higher_ballot_number(ballot_number) and self.promised_round == ballot_number:
+            #     if ballot_number[1] == self.serverID:
+            #         accepted_round = eval(promise_data[2]+1)
+            #         accepted_value = eval(promise_data[3])
+            #         if accepted_round > self.last_accepted_round:
+            #             self.last_accepted_round = accepted_round
+            #             self.accepted_value = accepted_value
+            #             accept_message = 'ACCEPT ' + str(ballot_number) + ' ' + str(accepted_value)
+            #             client_socket.sendall(bytes(accept_message, 'utf-8'))
+            #             # self.my_socket.sendall(bytes(accept_message, 'utf-8'))
+
+            elif data.startswith('ACCEPT'):
+                accept_data = data.split(' ', 2)
+                ballot_number = eval(accept_data[1])
+                if self.is_higher_ballot_number(ballot_number):
+                    accepted_value = eval(accept_data[2])
+                    self.accepted_round = ballot_number
+                    self.accepted_value = accepted_value
+                    decision_message = 'DECISION ' + str(ballot_number) + ' ' + str(accepted_value)
+                    for client_socket in self.clientSockets:
+                        client_socket.sendall(bytes(decision_message, 'utf-8'))
+                    self.append_to_log(accepted_value)
+                    self.apply_operation(accepted_value)
+                    self.remove_pending_operation(accepted_value)
+
+            elif data.startswith('DECISION'):
+                decision_data = data.split(' ', 2)
+                ballot_number = eval(decision_data[1])
+                decision_value = eval(decision_data[2])
+                if self.is_higher_ballot_number(ballot_number):
+                    self.append_to_log(decision_value)
+                    self.apply_operation(decision_value)
+                    self.remove_pending_operation(decision_value)
+
+    def is_higher_ballot_number(self, ballot_number):
+        if ballot_number[2] > len(self.log):
+            return True
+        elif ballot_number[2] == len(self.log) and ballot_number[1] > self.serverID:
+            return True
+        elif  ballot_number[0] > self.last_accepted_round:
+            return True
+        # if ballot_number[2] > len(self.log):
+        #     return True
+        # elif ballot_number[2] == len(self.log) and ballot_number[1] > self.serverID:
+        #     return True
+        # elif ballot_number[2] == len(self.log) and ballot_number[1] == self.serverID and ballot_number[0] > self.last_accepted_round:
+        #     return True
+        return False
+
+    def append_to_log(self, operation):
+        self.log.append(operation)
+
+    def apply_operation(self, operation):
+        if operation.startswith('POST'):
+            operation_data = operation.split(' ', 3)
+            username = operation_data[1]
+            title = operation_data[2]
+            content = operation_data[3]
+            self.blog.make_post(username, title, content)
+        elif operation.startswith('COMMENT'):
+            operation_data = operation.split(' ', 3)
+            username = operation_data[1]
+            title = operation_data[2]
+            comment = operation_data[3]
+            self.blog.comment_on_post(username, title, comment)
+
+    def remove_pending_operation(self, operation):
+        self.pending_operations.remove(operation)
+
+
 if __name__ == '__main__':
-    # Create 5 server
-    server1 = Server(str('P' + sys.argv[1]))
-
-    # Start clients
+    # server1 = Server(str('P' + sys.argv[1]))
+    server1 = Server(int(sys.argv[1]))
     server1.start()
-
